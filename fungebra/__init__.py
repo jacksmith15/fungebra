be878e7c6a29d7c2f8d28d2d91535c6536b73b42
Jack Smith <jack.smith@elucidata.co.uk>
2020-01-11 14:36:10 +0000

    Move tests to test module

--- fungebra/__init__.py
+++ fungebra/__init__.py
@@ -1,3 +1,4 @@
+from collections.abc import Mapping
 from functools import partial, reduce, update_wrapper
 import operator
 from typing import Callable, Iterable, Union
@@ -10,7 +11,7 @@ identity: Callable = lambda _: _
 
 
 def compose(*functions: Callable) -> Callable:
-    """Composes passed functions."""
+    """Composes arbitrary number of functions."""
     def _compose_pair(outer: Callable, inner: Callable) -> Callable:
         return lambda *args, **kwargs: outer(inner(*args, **kwargs))
     return reduce(_compose_pair, functions, identity)
@@ -33,8 +34,11 @@ class Function:
     def __hash__(self):
         return hash(self.func)
 
+    def __repr__(self):
+        return f"Function({repr(self.func)})"
+
     def partial(self, *args, **kwargs):
-        return F(partial(self, *args, **kwargs))
+        return F(partial(self.func, *args, **kwargs))
 
     @property
     def map(self):
@@ -68,22 +72,25 @@ class Function:
     def __ror__(self, other):
         if callable(other):
             return self + other
-        if isinstance(other, expand):
-            return self(*other.wrapped)
+        if isinstance(other, Args):
+            return self(*other.args, **other.kwargs)
         return self(other)
 
     def __eq__(self, other):
         return self.func == F(other).func
 
+    def __lshift__(self, input_args):
+        if isinstance(input_args, Args):
+            return self.partial(*input_args.args, **input_args.kwargs)
+        if isinstance(input_args, Mapping):
+            return self.partial(**input_args)
+        return self.partial(*input_args)
 
-class expand:
-    def __init__(self, args: Iterable):
-        self.wrapped = args
 
-    def __getattr__(self, attr):
-        if attr in self.__dict__:
-            return getattr(self, attr)
-        return getattr(self.wrapped, attr)
+class Args:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
 
 
 # Allow options for importing.
@@ -92,57 +99,3 @@ F = Func = Function  # pylint: disable=invalid-name
 
 def pipeline(*funcs):
     return reduce(operator.or_, map(F, funcs), F(identity))
-
-
-if __name__ == "__main__":
-    def add(*args):
-        return sum(args)
-
-    def double(number):
-        return number * 2
-
-    assert F(add).partial(1).partial(2, 3).partial(4)() == 10
-    assert F(add).partial(1).partial(2, 3)(4) == 10
-
-    assert F(add).compose(double)(1, 2, 3, 4) == 20
-    add_then_double: Callable = F(double) + add
-    assert add_then_double(1, 2, 3, 4) == 20
-    double_then_sum: Callable = F(double).map.partial([1, 2, 3, 4]).compose(sum)() == 20
-
-    assert F(add).reduce()([1, 2, 3, 4]) == 10
-    assert F(double).map.partial([1, 2, 3, 4]).compose(F(add).reduce())() == 20
-    assert F(double).map.reduce(operator.add)([1, 2, 3, 4]) == 20
-
-    test_pipeline: Callable = pipeline(F(double).map, F(add).reduce())
-    assert test_pipeline([1, 2, 3, 4]) == 20
-
-    assert (F(double) + sum)([1, 2, 3, 4]) == 20
-    assert (double + F(sum))([1, 2, 3, 4]) == 20
-    assert (F(sum) | double)([1, 2, 3, 4]) == 20
-    assert (sum | F(double))([1, 2, 3, 4]) == 20
-
-    get_double_sum_string: Callable = F(identity) | F(double).map | sum | str
-    assert get_double_sum_string([1, 2, 3, 4]) == "20"
-
-    def even(number):
-        return not number % 2
-
-    assert (F(even).filter() | sum)([1, 2, 3, 4]) == 6
-
-    @F
-    def negative(number):
-        return -number
-
-    assert negative.map.reduce(operator.add)([1, 2, 3, 4]) == -10
-    assert negative.map.filter(even).reduce(operator.add)([1, 2, 3, 4]) == -6
-
-    assert (negative.map | F(operator.add).reduce())([1, 2, 3, 4]) == -10
-    assert [1, 2, 3, 4] | (negative.map | list) == [-1, -2, -3, -4]
-
-    try:
-        [1, 2, 3, 4] | F(add)
-    except TypeError:
-        pass
-    else:
-        assert False
-    assert expand([1, 2, 3, 4]) | F(add) == 10
---
+++ tests/test_fungebra.py
@@ -0,0 +1,166 @@
+import operator
+
+import pytest
+
+from fungebra import Args, F, identity, pipeline
+
+
+def add(*args):
+    return sum(args)
+
+
+def double(number):
+    return number * 2
+
+
+def increment(number):
+    return number + 1
+
+
+def even(number):
+    return not number % 2
+
+
+class TestPartialApplication:
+    @staticmethod
+    def test_full_arguments_with_chaining():
+        assert F(add).partial(1).partial(2, 3).partial(4)() == 10
+
+    @staticmethod
+    def test_partial_arguments_with_chaining():
+        assert F(add).partial(1).partial(2, 3)(4) == 10
+
+    @staticmethod
+    def test_partial_application_using_lshift_operator():
+        func = F(add) << (1, 2, 3, 4)
+        assert func() == 10
+
+    @staticmethod
+    def test_lshift_partial_application_chaining():
+        func = F(add) << (1, 2) << (3, 4)
+        assert func() == 10
+
+    @staticmethod
+    def test_lshift_partial_application_argument_ordering():
+        func = F(operator.sub) << (1,) << (3,)
+        assert func() == -2
+
+    @staticmethod
+    def test_lshift_partial_application_with_kwargs_from_dict():
+        reverse_sorted = F(sorted) << {"key": operator.neg}
+        assert reverse_sorted([1, 2, 3]) == [3, 2, 1]
+
+    @staticmethod
+    def test_lshift_partial_application_with_kwargs_in_args_object():
+        func = F(sorted) << Args([1, 2, 3], key=operator.neg)
+        assert func() == [3, 2, 1]
+
+
+class TestFunctionComposition:
+
+    @staticmethod
+    def test_function_composition_via_method():
+        double_sum = F(sum).compose(double)
+        assert double_sum([1, 2, 3]) == 12
+
+    @staticmethod
+    def test_function_composition_via_left_add_operator():
+        double_sum = F(double) + sum
+        assert double_sum([1, 2, 3]) == 12
+
+    @staticmethod
+    def test_function_composition_via_right_add_operator():
+        double_sum = double + F(sum)
+        assert double_sum([1, 2, 3]) == 12
+
+    @staticmethod
+    def test_function_composition_via_pipe_method():
+        double_sum = F(sum).pipe(double)
+        assert double_sum([1, 2, 3]) == 12
+
+    @staticmethod
+    def test_function_composition_via_left_pipe_operator():
+        double_sum = F(sum) | double
+        assert double_sum([1, 2, 3]) == 12
+
+    @staticmethod
+    def test_function_composition_via_right_pipe_operator():
+        double_sum = sum | F(double)
+        assert double_sum([1, 2, 3]) == 12
+
+    @staticmethod
+    def test_function_composition_via_pipeline():
+        double_sum = pipeline(double, sum)
+        assert double_sum([1, 2, 3]) == 12
+
+
+class TestMapFilterReduce:
+
+    @staticmethod
+    def test_map_property():
+        map_double = F(double).map | list
+        assert map_double([1, 2, 3]) == [2, 4, 6]
+
+    @staticmethod
+    def test_filter_on_self():
+        filter_func = F(even).filter() | list
+        assert filter_func([1, 2, 3]) == [2]
+
+    @staticmethod
+    def test_filter_with_other():
+        filter_func = F(identity).filter(even) | list
+        assert filter_func([1, 2, 3]) == [2]
+
+    @staticmethod
+    def test_reduce_on_self():
+        reduce_func = F(operator.add).reduce()
+        assert reduce_func([1, 2, 3]) == 6
+
+    @staticmethod
+    def test_reduce_with_other():
+        reduce_func = F(identity).reduce(operator.add)
+        assert reduce_func([1, 2, 3]) == 6
+
+    @staticmethod
+    def test_map_chained_to_filter():
+        map_filter = F(increment).map.filter(even) | list
+        assert map_filter([1, 2, 3]) == [2, 4]
+
+    @staticmethod
+    def test_map_chained_to_reduce():
+        map_reduce = F(double).map.reduce(operator.add)
+        assert map_reduce([1, 2, 3]) == 12
+
+    @staticmethod
+    def test_map_filter_reduce():
+        map_filter_reduce = F(increment).map.filter(even).reduce(operator.add)
+        assert map_filter_reduce([1, 2, 3]) == 6
+
+
+class TestArgumentPipeline:
+
+    @staticmethod
+    def test_piping_single_argument():
+        assert [3, 2, 1] | F(sorted) == [1, 2, 3]
+
+    @staticmethod
+    def test_piping_multiple_arguments_via_args_object():
+        assert Args([1, 2, 3], key=operator.neg) | F(sorted) == [3, 2, 1]
+
+    @staticmethod
+    def test_piping_arguments_into_pipeline_function():
+        func = pipeline(F(double).map, sum, str)
+        assert [1, 2, 3] | func == "12"
+
+    @staticmethod
+    def test_piping_arguments_into_pipeline_expression():
+        assert [1, 2, 3] | (F(double).map | sum | str) == "12"
+
+
+def test_decorator_usage():
+    @F
+    def negative(number):
+        return -number
+
+    func = negative.map.filter(even) | next
+    assert func([1, 2, 3]) == -2
